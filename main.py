3f7fc307354d760b516368c47c71c520404bf79e
ふぁ <yuki@yuki0311.com>
2024-05-27 12:24:18 +0900

    update pytorch
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import io
 import logging
+import os
 import warnings
 import xml.etree.ElementTree as ET
 from logging import getLogger
 
-import cv2
 import numpy as np
 import requests
 import torch
@@ -28,7 +29,7 @@ class Settings(BaseSettings):
     username: str = Field()
     password: str = Field()
     path: str = Field()
-    score_threshold: float = Field(default=0.8)
+    score_threshold: float = Field(default=0.5)
     invisible_tags: str = Field(default="Tagged by deepdanbooru v1.0.0")
 
 
@@ -40,25 +41,35 @@ class DeepDanbooru:
         model.eval()
         model.half()
         model.cuda()
+        return model
 
     @staticmethod
-    def preprocess_image(img: np.ndarray, size=512, pad_color=0):
-        w, h = img.size
-        ratio = min(size / w, size / h)
-        new_w, new_h = int(w * ratio), int(h * ratio)
-        resized_img = img.resize((new_w, new_h), Image.ANTIALIAS)
-        new_img = Image.new("RGB", (size, size), pad_color)
-        new_img.paste(resized_img, ((size - new_w) // 2, (size - new_h) // 2))
-        return new_img
+    def preprocess_image(image: Image.Image, size=(512, 512)):
+        pic = image.convert("RGB").resize(size, Image.Resampling.LANCZOS)
+        return np.expand_dims(np.array(pic, dtype=np.float32), 0) / 255.0
 
     @staticmethod
-    def predict_and_parse(model, img, score_threshold):
+    def predict_and_parse(
+        model: DeepDanbooruModel,
+        img: np.ndarray,
+        score_threshold: float,
+    ):
         with torch.no_grad(), torch.autocast("cuda"):
             x = torch.from_numpy(img).cuda()
             y = model(x)[0].detach().cpu().numpy()
             for n in tqdm.tqdm(range(10)):
                 model(x)
-        return [model.tags[i] for i, p in enumerate(y) if p >= score_threshold]
+
+        rating_dict = {}
+        probability_dict = {}
+        for tag, probability in zip(model.tags, y):
+            if tag.startswith("rating:"):
+                rating_dict[tag] = probability
+            elif probability >= score_threshold:
+                probability_dict[tag] = probability
+
+        rating = max(rating_dict.keys(), key=lambda k: rating_dict[k])
+        return list(probability_dict.keys()) + [rating]
 
 
 class NextCloud:
@@ -96,7 +107,9 @@ class NextCloud:
                 elem = []
                 for tag in tags:
                     tag_elem = response.find(f".//{tag}", tag_namespace)
-                    if tag_elem.text is None:
+                    if tag_elem is None:
+                        raise Exception(f"Tag {tag} not found")
+                    elif tag_elem.text is None:
                         tag_elems = response.findall(f".//{tag}/*", tag_namespace)
                         elem.append([tag_elem.text for tag_elem in tag_elems])
                     else:
@@ -138,7 +151,11 @@ class NextCloud:
         return res
 
     def create_tag(
-        self, name, user_visible=True, user_assignable=True, can_assign=True
+        self,
+        name,
+        user_visible=True,
+        user_assignable=True,
+        can_assign=True,
     ):
         self.session.request(
             "POST",
@@ -188,25 +205,28 @@ class TagManager:
         raise Exception("Tag not found")
 
     class ModelManager:
-        def __init__(self, repo_id: str, filename: str, logger=None):
-            self.repo_id = repo_id
+        def __init__(self, filename: str, url: str, logger=None):
             self.filename = filename
-            self.tagger_model = None
+            self.url = url
+            self.model = None
             self.tagger_tags = None
             self.logger = logger or getLogger(__name__)
 
-        def load_model(self):
-            if self.tagger_model is None:
+        def load_model(self) -> DeepDanbooruModel:
+            if self.model is None:
                 self.logger.info("Loading model")
-                self.tagger_model, self.tagger_tags = DeepDanbooru.load_model(
-                    repo_id=self.repo_id,
+                if not os.path.exists(self.filename):
+                    torch.hub.download_url_to_file(self.url, self.filename)
+                self.model = DeepDanbooru.load_model(
                     filename=self.filename,
                 )
                 self.logger.info("Model loaded")
-            return self.tagger_model, self.tagger_tags
+            return self.model
 
 
-def remove_all_tags():
+def remove_all_tags(client: NextCloud):
+    images = client.list(env.path)
+    tag_manager = TagManager(client)
     for _, _, id, _, tags in images:
         for tag_name in tags:
             tag_id = tag_manager.get_tag_id(tag_name, hidden=False)
@@ -222,10 +242,12 @@ if __name__ == "__main__":
     logger.info("Starting DeepNextcloud")
 
     client = NextCloud(env.url, env.username, env.password)
+    # remove_all_tags(client)
+
     images = client.list(env.path)
     model_manager = TagManager.ModelManager(
-        repo_id="skytnt/deepdanbooru_onnx",
-        filename="deepdanbooru.onnx",
+        url="https://github.com/AUTOMATIC1111/TorchDeepDanbooru/releases/download/v1/model-resnet_custom_v3.pt",
+        filename="model-resnet_custom_v3.pt",
         logger=logger,
     )
 
@@ -240,15 +262,13 @@ if __name__ == "__main__":
         logger.info(f'Starting processing image "{image}"')
 
         content = client.download(id)
-        img = cv2.imdecode(np.frombuffer(content, np.uint8), cv2.IMREAD_COLOR)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = Image.open(io.BytesIO(content))
         img = DeepDanbooru.preprocess_image(img)
-        tagger_model, tagger_tags = model_manager.load_model()
+        model = model_manager.load_model()
 
         tags = DeepDanbooru.predict_and_parse(
-            tagger_model,
+            model,
             img,
-            tagger_tags,
             env.score_threshold,
         )
 
